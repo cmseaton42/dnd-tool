79a55870aafdf44b57e79ec76628f48684c688cc
Canaan Seaton <cmseaton42@gmail.com>
2020-08-17 06:55:30 -0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Switch, Route, Router, Redirect } from "react-router-dom";
+import { red } from "@material-ui/core/colors";
+import { createMuiTheme, ThemeProvider, responsiveFontSizes } from "@material-ui/core/styles";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Define global style constants
+const PRIMARY_COLOR = "#545863";
+const SECONDARY_COLOR = "#47A8BD";
+
+declare module "@material-ui/core/styles/createMuiTheme" {
+    interface Theme {
+        scrollbar: {
+            boxSizing: React.CSSProperties["boxSizing"];
+            overflow: React.CSSProperties["overflow"];
+            "&::-webkit-scrollbar-track": {
+                background: React.CSSProperties["background"];
+            };
+            "&::-webkit-scrollbar": {
+                width: React.CSSProperties["width"];
+            };
+            "&::-webkit-scrollbar-thumb": {
+                borderRadius: React.CSSProperties["borderRadius"];
+                background: React.CSSProperties["background"];
+                height: React.CSSProperties["height"];
+            };
+        };
+    }
+
+    // allow configuration using `createMuiTheme`
+    interface ThemeOptions {
+        scrollbar?: {
+            boxSizing?: React.CSSProperties["boxSizing"];
+            overflow?: React.CSSProperties["overflow"];
+            "&::-webkit-scrollbar-track"?: {
+                background: React.CSSProperties["background"];
+            };
+            "&::-webkit-scrollbar"?: {
+                width: React.CSSProperties["width"];
+            };
+            "&::-webkit-scrollbar-thumb"?: {
+                borderRadius: React.CSSProperties["borderRadius"];
+                background: React.CSSProperties["background"];
+                height: React.CSSProperties["height"];
+            };
+        };
+    }
 }
 
-export default App;
+// Generate custom Material UI theme
+let theme = createMuiTheme({
+    // Setup global color palette
+    palette: {
+        primary: {
+            main: PRIMARY_COLOR,
+        },
+        secondary: {
+            main: SECONDARY_COLOR,
+        },
+        error: {
+            main: red[600],
+        },
+    },
+    // Setup global scrollbar mixin
+    scrollbar: {
+        boxSizing: "border-box",
+        overflow: "auto",
+        "&::-webkit-scrollbar-track": {
+            background: "transparent",
+        },
+        "&::-webkit-scrollbar": {
+            width: "6px",
+        },
+        "&::-webkit-scrollbar-thumb": {
+            borderRadius: "3px",
+            background: "#cacad0",
+            height: "20px",
+        },
+    },
+    // Declare default overrides
+    overrides: {
+        // Custom tooltip
+        MuiTooltip: {
+            tooltip: {
+                backgroundColor: SECONDARY_COLOR,
+            },
+            arrow: {
+                color: SECONDARY_COLOR,
+            },
+        },
+    },
+});
+
+// Enable Material UI Responsive Fonts
+theme = responsiveFontSizes(theme);
+
+// Setup browser history API for
+//  usage with Google Analytics
+const history = createBrowserHistory();
+
+// Initialize Google Analytics page view tracking
+history.listen((location) => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
+// Setup Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS as string);
+
+const LoginPage: React.FC = () => <h1>Hello</h1>;
+
+// Setup App
+// @ts-ignore
+export const App: React.FC = () => {
+    return (
+        <React.Fragment>
+            {/* Setup CSS Baseline for Material UI */}
+            <CssBaseline />
+            {/* Provide Material UI Theme to App */}
+            <ThemeProvider theme={theme}>
+                {/* Instantiate Primary Application Router */}
+                <Router history={history}>
+                    <Switch>
+                        <Route exact path={"/"} component={LoginPage} />
+                        <Redirect to="/" />
+                    </Switch>
+                </Router>
+            </ThemeProvider>
+        </React.Fragment>
+    );
+};
