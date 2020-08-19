1c010bdf82088d5b98fed608765994e42ef43a8d
Canaan Seaton <cmseaton42@gmail.com>
2020-08-19 13:14:36 -0700

    Updates

---
+++ precache-manifest.fb4800d7c830253fc03cc08a63822ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a374e98d441dfb44eb6b14e2fb101cb3",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "9f93e50b5de02eff4038",
+    "url": "/dnd/static/js/2.7251341a.chunk.js"
+  },
+  {
+    "revision": "2b56c2c1139bbd5a27b8b425207a5091",
+    "url": "/dnd/static/js/2.7251341a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7770be39c9e1f358aa",
+    "url": "/dnd/static/js/main.532fe7bb.chunk.js"
+  },
+  {
+    "revision": "94c81701248427e67f8b",
+    "url": "/dnd/static/js/runtime-main.62edc368.js"
+  },
+  {
+    "revision": "246b879b335bebaefc2200710a0c2ee2",
+    "url": "/dnd/static/media/skull.246b879b.svg"
+  },
+  {
+    "revision": "19af7b51c14cd37f41d8995d557ccc5f",
+    "url": "/dnd/static/media/swords.19af7b51.svg"
+  }
+]);
